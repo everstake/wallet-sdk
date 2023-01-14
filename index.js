a6ca4d7b2ff3510a2cceebec04245c1b12ded370
yevheniimolodyk <y.molodyk@everstake.one>
2023-01-14 21:27:12 +0200

    change import

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-export * as Polygon from './polygon.js';
-export * as Cosmos from './cosmos.js';
-export * as Solana from './solana.js';
+import * as Polygon from './polygon.js';
+import * as Cosmos from './cosmos.js';
+import * as Solana from './solana.js';
+
+export { Polygon, Cosmos, Solana };
