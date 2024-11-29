62edb5515e2f5e71a7c43e95c5d1e1be5dc808f3
Messer4 <mikle0497@gmail.com>
2024-11-29 15:10:07 +0200

    feat: remove ofchain stats for solana

--- src/solana/constants/errors.ts
+++ src/solana/constants/errors.ts
@@ -2,7 +2,6 @@ export const ERROR_MESSAGES = {
   CONNECTION_ERROR: 'An error occurred while connecting to the network',
   MIN_AMOUNT_ERROR: 'Min Amount {0}',
   CREATE_ACCOUNT_ERROR: 'An error occurred while creating the account',
-  INVALID_TOKEN_ERROR: 'Please create or use correct token',
   DELEGATE_ERROR: 'An error occurred while delegating the stake',
   DEACTIVATE_ERROR: 'An error occurred while deactivating the stake',
   WITHDRAW_ERROR: 'An error occurred while withdrawing the stake',
