6c9c5da80dfdc392c05fe7cf8ad5a064917ee557
Vadym Mozharivsky <v.mozharivsky@everstake.one>
2024-12-12 11:04:19 +0200

    berrachain

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './ethereum';
 export * from './solana';
+export * from './berrachain';
 export * from './solana/stakeAccount';
 
 export * from './utils';
@@ -7,6 +8,8 @@ export * from './utils/api';
 
 export * from './ethereum/constants';
 export * from './solana/constants';
+export * from './berrachain/constants';
 
 export * from './ethereum/types';
 export * from './solana/types';
+export * from './berrachain/types';
---
+++ src/utils/constants/errors.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.COMMON_ERROR_MESSAGES = void 0;
+exports.COMMON_ERROR_MESSAGES = {
+    UNKNOWN_ERROR: 'An unknown error occurred',
+    TOKEN_ERROR: 'Please create or use correct token',
+};
