fca74ac565f8d9cb19d71b6046599b666d7cab7e
d.berezhnenko <d.berezhnenko@everstake.one>
2025-03-19 15:59:55 +0200

    added tests for sui

--- src/sui/__tests__/index.ts
+++ src/sui/__tests__/index.ts
@@ -1,12 +1,66 @@
 import { Sui } from '..';
+import { WalletSDKError } from '../../utils';
+import {
+  invalidGetStakeBalanceAddress,
+  selectNetworErrorkFixture,
+  selectNetworkSuccessFixture,
+  stakeAmount,
+} from '../__fixtures__';
+import { SuiNetworkType } from '../types';
 
-async function main() {
-  const suicli = new Sui('testnet');
-  const pk = process.env.SUI_PK;
+describe('selectNetwork', () => {
+  selectNetworkSuccessFixture.forEach(({ description, args, result }) => {
+    it(description, () => {
+      const sui = new Sui(args.network as SuiNetworkType);
 
-  const depTx = await suicli.stake('1');
+      expect(sui.validatorAddress).toBe(result.validatorAddress);
 
-  console.log(depTx);
-}
+      const suiInstance = sui.selectNetwork(args.network as SuiNetworkType);
 
-main();
+      expect(suiInstance.validatorAddress).toBe(result.validatorAddress);
+    });
+  });
+
+  selectNetworErrorkFixture.forEach(({ description, args, error }) => {
+    it(description, () => {
+      expect(() =>
+        new Sui().selectNetwork(args.network as SuiNetworkType),
+      ).toThrow(error);
+    });
+  });
+});
+
+describe('stake', () => {
+  stakeAmount.forEach(({ description, args, error }) => {
+    it(description, async () => {
+      const sui = new Sui();
+      if (error.message) {
+        const { message, code } = error;
+        try {
+          await sui.stake(args.amount);
+        } catch (error) {
+          const e = error as WalletSDKError;
+          expect(e.message).toBe(message);
+          expect(e.code).toBe(code);
+        }
+      } else {
+        await expect(await sui.stake(args.amount)).not.toThrow();
+      }
+    });
+  });
+});
+
+describe('getStakeBalanceByAddress', () => {
+  invalidGetStakeBalanceAddress.forEach(({ description, args, error }) => {
+    it(description, async () => {
+      const errorMessage = error;
+      const sui = new Sui();
+      try {
+        await sui.getStakeBalanceByAddress(args.address);
+      } catch (error) {
+        const e = error as WalletSDKError;
+        expect(e.message).toBe(errorMessage);
+      }
+    });
+  });
+});
--- src/sui/constants/error.ts
+++ src/sui/constants/error.ts
@@ -6,9 +6,11 @@
 export const ERROR_MESSAGES = {
   ADDRESS_FORMAT_ERROR: 'Invalid Sui address format',
   STAKE_BALANCE_ERROR: 'Error getting stake balance',
+  MIN_STAKE_AMOUNT_ERROR: 'Amount is less than minimum stake amount',
   STAKE_ERROR: 'Error staking',
   USER_BALANCES_ERROR: 'Error getting user balances',
   UNSTAKE_ERROR: 'Error unstaking',
+  NETWORK_NOT_SUPPORTED: 'Unsupported network',
 };
 
 export const ORIGINAL_ERROR_MESSAGES = {};
