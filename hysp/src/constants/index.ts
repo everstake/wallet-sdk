e2ba94fff550e813692b575dd815ba3a53ed55db
d.berezhnenko <d.berezhnenko@everstake.one>
2025-12-01 20:21:45 +0200

    add apy function

--- hysp/src/constants/index.ts
+++ hysp/src/constants/index.ts
@@ -26,3 +26,6 @@ export const NETWORKS: NetworksMap = {
 
 export const ZeroReferrer =
   '0x0000000000000000000000000000000000000000000000000000000000000000';
+
+export const APY_API_ENDPOINT = 'https://api-prod.midas.app/api/data/apys';
+export const APY_VAULT_KEY = 'mevusd';
